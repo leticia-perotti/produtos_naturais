015d010cf933d9a7779b265ad46171b8b3b8ab4e
Leticia <col.leticia.perotti@uniuv.edu.br>
2021-05-03 15:52:31 -0300

    teste

---
+++ natural_cha/menu.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+Natural Chá
+    </a>
+
+    <ul class="nav justify-content-end">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+    </ul>
+</nav>
+
+
+
