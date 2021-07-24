a9716a3947aa24a159fe891010b683ba0acf33dd
leticia-perotti <leticiadanieleperotti@gmail.com>
2021-07-24 14:33:14 -0300

    24.07

--- listagem/vizualizar.php
+++ listagem/vizualizar.php
@@ -1,5 +1,37 @@
 
-<!-- Modal de serviços -->
+<!-- Modal de adicionar produto -->
+<style>
+    .modal-header {
+        background-color: blanchedalmond;
+    }
+    .modal-title{
+        font-family: 'Comfortaa', cursive;
+        font-family: 'Pattaya', sans-serif;
+        font-size: x-large;
+    }
+    #imagem_modal{
+        width: 45%;
+        float :left;
+    }
+    #imagem_modal:hover{
+        opacity: 80%;
+    }
+    #lado{
+        float: right;
+
+    }
+    #botao{
+        background-color: blanchedalmond;
+        color: black;
+        border-style: none;
+        float: right;
+    }
+    label{
+        color: #5d5d5d;
+    }
+
+</style>
+
 <div class="modal fade" id="vizualizar_produto" tabindex="-1" role="dialog" aria-labelledby="vizualizar_produtoLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -10,16 +42,18 @@
                 </button>
             </div>
             <div class="modal-body">
-                    <h1 class="titulo">Camomila</h1>
-                    <img src="../fotos/camomila.jpg" width="30">
-                    <div class="form-group row">
+
+                    <img src="../fotos/camomila.jpg" title="Camomila" id="imagem_modal">
+                    <div class="form-group row" id="lado">
                         <label class="col-sm-2 col-form-label">Valor</label>
+                        <br>
                         <div class="col-sm-10">
                             <input type="text" readonly class="form-control-plaintext" value="R$ 3.00">
                         </div>
                     </div>
-                    <div class="form-group row">
+                    <div class="form-group row" id="lado">
                         <label class="col-sm-2 col-form-label">Descrição</label>
+                        <br>
                         <div class="col-sm-10">
                             <input type="text" readonly class="form-control-plaintext" value="Pacote com 50g">
                         </div>
@@ -33,8 +67,8 @@
                 </div>
 
                     <div class="form-group">
-                        <button type="submit" class="btn btn-success" >Salvar</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                        <button type="submit" class="btn btn-light" id="botao" >Adicionar</button>
+
                     </div>
 
             </div>
