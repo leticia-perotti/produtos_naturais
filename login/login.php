c3467a96dccaf388eb9152f49ccc572f9c691273
leticia-perotti <leticiadanieleperotti@gmail.com>
2021-08-25 19:06:09 -0300

    25.8

--- login/login.php
+++ login/login.php
@@ -1,6 +1,7 @@
 <?php
 include_once ("../conexao.php");
 include(__ROOT__ . '/documentacao.php');
+
 ?>
 
 <!doctype html>
@@ -11,7 +12,19 @@ include(__ROOT__ . '/documentacao.php');
     <link rel="icon" href="../fotos/logo_mini.png">
     <title>Natural Chá</title>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js" integrity="sha256-yE5LLp5HSQ/z+hJeCqkz9hdjNkk1jaiGG0tDCraumnA=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js" integrity="sha256-sPB0F50YUDK0otDnsfNHawYmA5M0pjjUf4TvRJkGFrI=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/additional-methods.min.js" integrity="sha256-vb+6VObiUIaoRuSusdLRWtXs/ewuz62LgVXg2f1ZXGo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/localization/messages_pt_BR.min.js" integrity="sha256-XPVq9FOi0rZTuUUM1OBNwLj/HPADmvgTT+KSuoDqjjw=" crossorigin="anonymous"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('.cpf').mask('000.000.000-00', {clearIfNotMatch:true});
+            $("form").validate();
+        });
+    </script>
 <style>
+
 
     .grid{
         alignment: center;
@@ -66,7 +79,7 @@ include(__ROOT__ . '/documentacao.php');
 
           <div class="form-group">
               <label for="cpf">CPF</label>
-              <input type="number" class="form-control" name="cpf" id="cpf" required>
+              <input type="text" class="form-control cpf" name="cpf" id="cpf" required>
           </div>
 
        <div class="form__field">
@@ -92,6 +105,30 @@ include(__ROOT__ . '/documentacao.php');
       </a>
   </div>
 
-
-
 </body>
+<script>
+    $(document).ready(function() {
+        $('.jsonForm').ajaxForm({
+            dataType: 'json',
+            success: function (data) {
+                if (data.status==true) {
+                    iziToast.success({
+                        message: data.mensagem
+                    });
+                    $('.jsonForm').trigger('reset');
+                }else {
+                    iziToast.error({
+                        message: data.mensagem
+                    });
+                }
+
+            },
+            error: function (data) {
+                iziToast.error({
+                    message: 'Servidor retornou erro'
+                });
+            }
+        });
+
+    });
+</script>
--- login/verificaLogin.php
+++ login/verificaLogin.php
@@ -2,23 +2,23 @@
 
 try{
 include_once("../conexao.php");
-include(__ROOT__ . '/documentacao.php');
 
 $email = $_POST['email'];
 $cpf = $_POST['cpf'];
 
 
-$conferir = $conexao->prepare('Select email, cpf from cliente where email=:email and cpf=:cpf');
+$conferir = $conexao->prepare('Select email, cpf, id from cliente where email=:email and cpf=:cpf');
 $conferir->bindParam(":email", $email);
 $conferir->bindParam(":cpf", $cpf);
 $conferir->execute();
 
+$achaID= $conferir->fetchObject();
+
 if ($conferir->rowCount() == 1) {
+    $cliente = $achaID-> id;
 
-    $cliente = $conferir-> lastInsertId();
-
-    $adicionaCliente = $conexao-> prepare('Update atendimento set cliente_idcliente =: cliente
-                                                    where idatendimento =:id ');
+    $adicionaCliente = $conexao-> prepare('Update atendimento set cliente_idclientes=:cliente
+                                                    where idatendimento=:id ');
     $adicionaCliente-> bindParam(':id', $_COOKIE['carrinho']);
     $adicionaCliente->bindParam(':cliente', $cliente);
     $adicionaCliente->execute();
